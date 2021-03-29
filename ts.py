f2655e9a6f28616c6c84265e5e3f554e95d96272
Landen Doty <landot2002@gmail.com>
2021-03-29 00:57:17 -0500

    Add files via upload

---
+++ ts.py
@@ -0,0 +1,18 @@
+#Taylor Series Approx.
+
+def factorial(x):
+    fact = 1
+    for i in range(1,x+1):
+        fact = fact * i
+    return(fact)
+
+def ts(n,x):
+    e=0
+    for i in range(0,n):
+        e=e+((x**i)/(factorial(i)))
+    print("e^" + str(x) + " is approximately: " + str(e))
+
+approx_e_by = int(input("How many terms to approximate e^x by: "))
+e_to_the = int(input("Value of x: "))
+
+ts(approx_e_by, e_to_the)
